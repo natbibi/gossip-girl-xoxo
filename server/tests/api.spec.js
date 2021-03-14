826ef3a450218f56c7c3d62fd4e578ca3ac51b5f
chris westerduin <chriswesterduin@outlook.com>
2021-03-14 00:42:44 +0000

    update api tests

--- server/tests/api.spec.js
+++ server/tests/api.spec.js
@@ -2,16 +2,20 @@ const request = require("supertest");
 // import server
 const server = require("../server");
 const data = require("../data.json");
+const postsData = data.posts
 
 const testPost = {
-  date: "Fri Mar 12 2021 22:39:25 GMT+0000 (Greenwich Mean Time)",
-  text: "post text here",
-  giphy: null,
-};
+    id: 2,
+    text: 'post text here',
+    date: 'Fri Mar 12 2021 22:39:25 GMT+0000 (Greenwich Mean Time)',
+    comments: [],
+    reactions: { happy: 0, funny: 0, unhappy: 0 },
+    giphy: null
+  }
 
 const testComment = {
   date: "Fri Mar 12 2021 22:39:25 GMT+0000 (Greenwich Mean Time)",
-  text: "post text here",
+  text: "comment text here",
 };
 
 describe("API server", () => {
@@ -35,16 +39,16 @@ describe("API server", () => {
   });
 
   it("responds to get get /posts with 200 and all posts data", (done) => {
-    request(api).get("/posts").expect(200).expect(data, done);
+    request(api).get("/posts").expect(200).expect(postsData, done);
   });
 
   it("responds to get get /posts/:id with 200 and a specific post data", (done) => {
-    request(api).get("/posts/1").expect(200).expect(data[0], done);
+    request(api).get("/posts/1").expect(200).expect(postsData[0], done);
   });
 
-  it("responds to post /posts/ with 200 and creates new post", (done) => {
+  it("responds to post /posts with 200 and creates new post", (done) => {
     request(api)
-      .post("/posts/new")
+      .post("/posts")
       .send(testPost)
       .expect(200)
       .expect({ id: 2, ...testPost }, done);
@@ -55,11 +59,11 @@ describe("API server", () => {
       .patch("/posts/1/comments")
       .send(testComment)
       .expect(204, done)
-    // request get posts/1 comments to include testComment
   });
 
   it("responds to patch posts/:id/reactions with 200 and updates post reactions", (done) => {
-    request(api).patch("/posts/1/reaction").send("happy").expect(204, done);
-    // request get posts/1 reactions to include new reaction
+    request(api).patch("/posts/1/reactions")
+    .send({reaction:"happy"})
+    .expect(204, done);
   });
 });
